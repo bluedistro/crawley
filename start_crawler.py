ec60f07c588c351d56a90bdebd954a3207c1865e
captainburnaby <bineykingsley36@gmail.com>
2018-05-07 20:36:33 +0000

    petty modifications

--- start_crawler.py
+++ start_crawler.py
@@ -3,7 +3,7 @@ from endpoint import endpoint
 
 # spontaneously start the crawler to crawl for ever
 try:
-    sites = endpoint(project_name='geo_gis', site='http://www.opengeospatial.org/')
+    sites = endpoint(project_name='geo_gis', site='https://news.ycombinator.com')
     sites.create_workers()
     sites.crawl()
 except Exception as e:
